09483273aed45c51286c7bf6df1a767d64220337
Vishal Pimpale <pimpalevishal@gmail.com>
2025-04-21 13:50:50 +0000

    Update CI/CD workflows and Terraform config for ECR repo strapi-appp-vishal

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "image_name" {
+  type = string
+}
+
 resource "aws_instance" "vishal-strapi-app" {
   ami           = "aami-084568db4383264d4"
   instance_type = "t2.medium"
@@ -10,8 +14,8 @@ resource "aws_instance" "vishal-strapi-app" {
   user_data = <<-EOF
               #!/bin/bash
               aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${var.ecr_registry}
-              docker pull ${var.ecr_registry}/strapi-app:${var.image_tag}
-              docker run -d -p 1337:1337 ${var.ecr_registry}/strapi-app:${var.image_tag}
+              docker pull ${var.ecr_registry}/${var.image_name}:${var.image_tag}
+              docker run -d -p 1337:1337 ${var.ecr_registry}/${var.image_name}:${var.image_tag}
               EOF
 
   security_groups = [aws_security_group.strapi_sg.name]
