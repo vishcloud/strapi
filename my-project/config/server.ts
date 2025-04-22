69d02a3e493291d72a633847eea10b46b0656087
vishcloud <164619565+vishcloud@users.noreply.github.com>
2025-04-22 13:32:32 +0530

    Update server.ts

--- my-project/config/server.ts
+++ my-project/config/server.ts
@@ -2,6 +2,8 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), [
+      'randomKey1_ReplaceWithStrongRandomString',
+      'randomKey2_ReplaceWithStrongRandomString'
   },
 });
