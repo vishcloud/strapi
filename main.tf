f63e302abec9b181c4e827bd0361916c744d3ab2
vishcloud <164619565+vishcloud@users.noreply.github.com>
2025-04-23 11:35:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,8 +32,8 @@ variable "ssh_key_name" {
 }
 
 # Security group for Strapi EC2 instance
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-security-group"
+resource "aws_security_group" "vishal_strapi_sg" {
+  name        = "vishal-strapi-security-group"
   description = "Security group for Strapi application"
   vpc_id      = var.vpc_id
 
@@ -78,7 +78,7 @@ resource "aws_security_group" "strapi_sg" {
   }
 
   tags = {
-    Name = "strapi-sg"
+    Name = "vishal_strapi_sg"
   }
 }
 
@@ -103,7 +103,7 @@ resource "aws_instance" "vishal_strapi_server" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.ssh_key_name
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+  vpc_security_group_ids = [aws_security_group.vishal_strapi_sg.id]
   subnet_id              = var.subnet_id
 
   root_block_device {
@@ -136,5 +136,5 @@ resource "aws_instance" "vishal_strapi_server" {
     Name = "vishal_strapi_server"
   }
 
-  depends_on = [aws_security_group.strapi_sg]
+  depends_on = [aws_security_group.vishal_strapi_sg]
 }
